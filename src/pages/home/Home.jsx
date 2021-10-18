6de01131a2efc166300c5c3b6183057a81b24cac
Yakushin103 <vyakushin103@yandex.ru>
2021-10-18 14:46:20 +0300

    Added request for get random list

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,34 @@
-import './home.scss'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 import Navbar from '../../components/navbar/Navbar'
 import Featured from '../../components/featured/Featured'
 import List from '../../components/list/List'
 
+import './home.scss'
+
 const Home = ({ type }) => {
+  const [lists, setLists] = useState([])
+  const [genre, setGenre] = useState(null)
+
+  useEffect(() => {
+    const getRandonList = async () => {
+      try {
+        const res = await axios.get(
+          `lists${type ? '?type=' + type : ''}${genre ? '&genre=' + genre : ''}`, {
+          headers: {
+            token: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNmFkMmZiNmFlNzNkODdhZTVjMWQ0MSIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2MzQzOTA5NTIsImV4cCI6MTYzNDgyMjk1Mn0.ATUakv9DOo-5yChoyQ_AiKPWq0YbogPO7Qr7P5c-b2k'
+          }
+        }
+        )
+        setLists(res.data)
+      } catch (err) {
+        console.log(err)
+      }
+    }
+    getRandonList()
+  }, [type, genre])
+
   return (
     <div className="home">
       <Navbar />
@@ -12,13 +36,14 @@ const Home = ({ type }) => {
       <Featured
         type={type}
       />
-
-      <List />
-      <List />
-      <List />
-      <List />
-      <List />
-      <List />
+      {
+        lists.map(list => (
+          <List
+            key={list._id}
+            list={list}
+          />
+        ))
+      }
     </div>
   )
 }
